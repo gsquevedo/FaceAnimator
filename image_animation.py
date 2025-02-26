bbba542986b4175dbc29693cf04c4d93576265f5
Gabriele Quevedo <54038952+gsquevedo@users.noreply.github.com>
2025-02-26 11:22:54 -0300

    ajustes

--- image_animation.py
+++ image_animation.py
@@ -14,7 +14,6 @@ import sys
 def is_executable():
     return getattr(sys, 'frozen', False) or hasattr(sys, '_MEIPASS')
 
-# Argumentos do programa
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--input_image", required=True, help="Path to image to animate")
 ap.add_argument("-c", "--checkpoint", required=True, help="Path to checkpoint")
@@ -23,7 +22,6 @@ args = vars(ap.parse_args())
 
 config_path = 'config/vox-256.yaml' if not is_executable() else './vox-256.yaml'
 
-# Carregar imagem de origem e checkpoints
 print("[INFO] Loading source image and checkpoint...")
 source_path = args['input_image']
 checkpoint_path = args['checkpoint']
@@ -36,7 +34,6 @@ source_image = imageio.imread(source_path)
 source_image = resize(source_image, (256, 256))[..., :3]
 generator, kp_detector = load_checkpoints(config_path=config_path, checkpoint_path=checkpoint_path)
 
-# Criação do diretório de saída
 if not os.path.exists('output'):
     os.mkdir('output')
 
@@ -44,7 +41,6 @@ relative = True
 adapt_movement_scale = True
 cpu = True
 
-# Configuração da câmera ou vídeo
 if video_path:
     cap = cv2.VideoCapture(video_path)
     print("[INFO] Loading video from the given path")
@@ -68,17 +64,18 @@ with torch.no_grad():
         ret, frame = cap.read()
         frame = cv2.flip(frame, 1)
         if ret:
-            # Obtenção do tamanho do quadro da webcam
             h, w, _ = frame.shape
 
-            if not video_path:  # Não aplica o corte quando for vídeo de entrada
-                # Corte central do quadro para 256x256
+            if not video_path:  
+                scale_factor = 1.5
+                new_size = int(256 * scale_factor)
+
                 center_h, center_w = h // 2, w // 2
-                cropped_frame = frame[center_h - 128:center_h + 128, center_w - 128:center_w + 128]
-                # Redimensionar o quadro cortado para o tamanho adequado
-                frame_resized = resize(cropped_frame, (256, 256))[..., :3]
+                cropped_frame = frame[max(0, center_h - new_size // 2):min(h, center_h + new_size // 2),
+                                      max(0, center_w - new_size // 2):min(w, center_w + new_size // 2)]
+
+                frame_resized = resize(cropped_frame, (256, 256), mode='reflect', anti_aliasing=True)[..., :3]
             else:
-                # Para vídeos, usa-se o frame original sem corte
                 frame_resized = resize(frame, (256, 256), mode='reflect', anti_aliasing=True)[..., :3]
 
             if count == 0:
@@ -86,14 +83,12 @@ with torch.no_grad():
                 source1 = torch.tensor(source_image1[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
                 kp_driving_initial = kp_detector(source1)
 
-            # Processar o quadro do vídeo
             frame_test = torch.tensor(frame_resized[np.newaxis].astype(np.float32)).permute(0, 3, 1, 2)
             driving_frame = frame_test
             if not cpu:
                 driving_frame = driving_frame.cuda()
             kp_driving = kp_detector(driving_frame)
 
-            # Normalização dos pontos de referência para aplicar movimentos faciais (como o sorriso)
             kp_norm = normalize_kp(kp_source=kp_source,
                                    kp_driving=kp_driving,
                                    kp_driving_initial=kp_driving_initial,
@@ -106,7 +101,6 @@ with torch.no_grad():
             im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
             joinedFrame = np.concatenate((cv2_source, im, frame_resized), axis=1)
 
-            # Exibir o quadro com a animação aplicada
             cv2.imshow('Test', joinedFrame)
             out1.write(img_as_ubyte(joinedFrame))
             count += 1
@@ -115,7 +109,6 @@ with torch.no_grad():
         else:
             break
 
-    # Libere os recursos de captura e vídeo
     cap.release()
     out1.release()
     cv2.destroyAllWindows()
